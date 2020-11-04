b903de1bf5b90de6c4eda61440d732a54530a86f
123survesh <123survesh@gmail.com>
2020-11-04 14:23:59 +0530

    Added two canvases, one for event based move call and other for requestAnimationFrame based

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-var canvas = document.getElementById('container');
-var context = canvas.getContext('2d');
+let canvas = document.getElementById('box-1');// canvas calls draw() from events directly
+let context = canvas.getContext('2d');
 
-var actionMap = {
+let rafCanvas = document.getElementById('box-2'); // canvas uses requestAnimationFrame for drawing
+let rafContext = rafCanvas.getContext('2d');
+
+let actionMap = {
     left: 1,
     right: 2,
     up: 3,
     down: 4,
     jump: 5
 }
-var keyMap = {
+let keyMap = {
     'ArrowLeft': 'left',
     'ArrowRight': 'right',
     'ArrowUp': 'up',
     'ArrowDown': 'down',
     ' ': 'jump'
 }
-var options = {};
+let options = {
+    keyDown: null,
+    updateStarted: false
+};
 
-var touchLog = {
+let keyPressTime = 0;
+
+let touchLog = {
     start: {
         x: 0,
         y: 0
@@ -30,8 +38,8 @@ var touchLog = {
 };
 
 options.canvas = {
-    h: window.innerHeight*0.8,
-    w: window.innerWidth*0.8,
+    h: window.innerHeight - 40,
+    w: window.innerWidth*0.5,
     border: 10,
 }
 
@@ -43,9 +51,7 @@ options.canvas.draw = function (context) {
 
 }
 
-options.box = {
-    x: options.canvas.border,
-    y: options.canvas.border,
+options.boxData = {
     h: 100,
     w: 100,
     fillStyle: '#ff9933',
@@ -54,104 +60,144 @@ options.box = {
         y: options.canvas.border
     },
     max: {
-        x: options.canvas.w - (options.canvas.border + 100),
-        y: options.canvas.h - (options.canvas.border + 100)
+        x: options.canvas.w - ((options.canvas.border) + 100),
+        y: options.canvas.h - ((options.canvas.border) + 100)
     },
-    moveBy : 25
+    moveBy : 25,
+    rafMoveBy: 5
+}
+
+options.box = {
+    x: options.canvas.border,
+    y: options.canvas.border,
+}
+
+options.rafBox = {
+    x: options.canvas.border,
+    y: options.canvas.border,
 }
 
 options.box.draw = function (context) {
     context.lineWidth = 1;
-    context.fillStyle = options.box.fillStyle;
-    context.fillRect(options.box.x, options.box.y, options.box.w, options.box.h);
+    context.fillStyle = options.boxData.fillStyle;
+    context.fillRect(options.box.x, options.box.y, options.boxData.w, options.boxData.h);
 }
 
-var layers = [];
+options.rafBox.draw = function (context) {
+    context.lineWidth = 1;
+    context.fillStyle = options.boxData.fillStyle;
+    context.fillRect(options.rafBox.x, options.rafBox.y, options.boxData.w, options.boxData.h);
+}
 
-layers.push('canvas');
-layers.push('box');
+let layers = ['canvas', 'box'];
+let rafLayers = ['canvas', 'rafBox'];
 
 canvas.height = options.canvas.h;
 canvas.width = options.canvas.w;
 
-compositior(context, layers, options);
+rafCanvas.height = options.canvas.h;
+rafCanvas.width = options.canvas.w;
+
+compositior(context, layers);
+compositior(rafContext, rafLayers);
 
 
-addListeners();
-function addListeners()
+
+(function addListeners()
 {
-	document.addEventListener("keydown", _handleKeyDown);
-	canvas.addEventListener("touchstart", _handleTouchStart);
-	canvas.addEventListener("touchend", _handleTouchEnd);
-	canvas.addEventListener("touchcancel", _handleTouchEnd);
-	canvas.addEventListener("touchmove", _handleTouchMove);
-}
+    document.addEventListener("keydown", _handleKeyDown);
+    document.addEventListener("keyup", _handleKeyUp);
+    canvas.addEventListener("touchstart", _handleTouchStart);
+    canvas.addEventListener("touchend", _handleTouchEnd);
+    canvas.addEventListener("touchcancel", _handleTouchEnd);
+    canvas.addEventListener("touchmove", _handleTouchMove);
+})();
 
 
-function move(direction, options) {
-    var action = actionMap[direction];
-    var distance = options.moveBy;
-    var toMove = {
-        x: options.x,
-        y: options.y
+function move(direction) {
+    let toMove = {
+        x: options.box.x,
+        y: options.box.y,
     }
-    switch (action) {
+    switch (actionMap[direction]) {
     case 1:
-        toMove.x = toMove.x - distance;
+        toMove.x = Math.max(options.boxData.min.x, toMove.x - options.boxData.moveBy);
         break;
     case 2:
-        toMove.x = toMove.x + distance;
+        toMove.x = Math.min(options.boxData.max.x, toMove.x + options.boxData.moveBy);
         break;
     case 3:
-        toMove.y = toMove.y - distance;
+        toMove.y = Math.max(options.boxData.min.y, toMove.y - options.boxData.moveBy);
         break;
     case 4:
-        toMove.y = toMove.y + distance;
+        toMove.y = Math.min(options.boxData.max.y, toMove.y + options.boxData.moveBy);
         break;
     }
-    if (checkBoundary(toMove, options)) {
-        options.x = toMove.x;
-        options.y = toMove.y;
-    }
+    options.box.x = toMove.x;
+    options.box.y = toMove.y;
 }
 
-function checkBoundary(to, options) {
-    var pass = false;
-    try {
-        if (to.x <= options.max.x && to.x >= options.min.x) {
-            if (to.y <= options.max.y && to.y >= options.min.y) {
-                pass = true;
-            }
+function updateRaf() {
+    if(options.keyDown !== null) {
+        let toMove = {
+            x: options.rafBox.x,
+            y: options.rafBox.y,
         }
-
-        return pass;
-    } catch (e) {
-        console.log(e);
-        return pass;
+        switch (actionMap[options.keyDown]) {
+            case 1:
+                toMove.x = Math.max(options.boxData.min.x, toMove.x - options.boxData.rafMoveBy);
+                break;
+            case 2:
+                toMove.x = Math.min(options.boxData.max.x, toMove.x + options.boxData.rafMoveBy);
+                break;
+            case 3:
+                toMove.y = Math.max(options.boxData.min.y, toMove.y - options.boxData.rafMoveBy);
+                break;
+            case 4:
+                toMove.y = Math.min(options.boxData.max.y, toMove.y + options.boxData.rafMoveBy);
+                break;
+        }
+        options.rafBox.x = toMove.x;
+        options.rafBox.y = toMove.y;
+        compositior(rafContext, rafLayers, options);
+        _drawDirectionText(rafContext, options.keyDown);
+        requestAnimationFrame(updateRaf);
     }
 }
 
-function draw(ctx, options) {
-    if (options.fillStyle) {
-        ctx.fillStyle = options.fillStyle;
-    }
-    ctx.fillRect(options.x, options.y, options.w, options.h);
-}
-
-function compositior(context, array, options) {
-    var aSize = array.length;
-    for (var i = 0; i < aSize; ++i) {
-        options[array[i]].draw(context);
+function compositior(context, array) {
+    let aSize = array.length;
+    let i = 0;
+    while(i<aSize) {
+        options[array[i++]].draw(context);
     }
 }
 
 function _handleKeyDown(event) {
-    var direction = keyMap[event.key];
+    
+    let direction = keyMap[event.key];
+    if(options.keyDown !== direction) {
+        options.keyDown = direction;
+    }
     if (direction) {
         event.preventDefault();
         move(direction, options.box);
         compositior(context, layers, options);
         _drawDirectionText(context, direction);
+    }
+    
+    if(options.updateStarted === false) {
+        updateRaf();
+        options.updateStarted = true;
+    }
+}
+
+function _handleKeyUp(event) {
+    let direction = keyMap[event.key];
+    if (direction === options.keyDown) {
+        event.preventDefault();
+        options.keyDown = null;
+        options.updateStarted = false;
     }
 }
 
@@ -165,7 +211,7 @@ function _handleTouchStart(event) {
 }
 
 function _handleTouchMove(event) {
-    var d = "";
+    let d = "";
     touchLog.move.x = event.touches[0].pageX - touchLog.start.x;
     touchLog.move.y = event.touches[0].pageY - touchLog.start.y;
     if (touchLog.preventPullDownRefresh) {
@@ -175,7 +221,7 @@ function _handleTouchMove(event) {
             event.preventDefault();
         }
     }
-    var xFlag = (Math.abs(touchLog.move.x) > Math.abs(touchLog.move.y));
+    let xFlag = (Math.abs(touchLog.move.x) > Math.abs(touchLog.move.y));
     if (Math.abs(touchLog.move.x) > 30 || Math.abs(touchLog.move.y) > 30) {
         touchLog.moveFlag = true;
         if (xFlag) {
@@ -202,6 +248,14 @@ function _handleTouchMove(event) {
         move(d, options.box);
         compositior(context, layers, options);
         _drawDirectionText(context, d);
+
+        if(options.keyDown !== d) {
+            options.keyDown = d;
+            if(options.updateStarted === false) {
+                updateRaf();
+                options.updateStarted = true;
+            }
+        }
     }
 
 }
@@ -217,6 +271,9 @@ function _handleTouchEnd(event) {
         move: { x: 0, y: 0 },
         moveFlag: false
     }
+
+    options.keyDown = null;
+    options.updateStarted = false;
 }
 
 function _drawDirectionText(context, direction)
@@ -225,3 +282,27 @@ function _drawDirectionText(context, direction)
 	context.textAlign = "center";
 	context.fillText(direction.toUpperCase(), canvas.width/2, canvas.height/2); 
 }
+
+
+// function checkBoundary(to, options) {
+//     let pass = false;
+//     try {
+//         if (to.x <= options.max.x && to.x >= options.min.x) {
+//             if (to.y <= options.max.y && to.y >= options.min.y) {
+//                 pass = true;
+//             }
+//         }
+
+//         return pass;
+//     } catch (e) {
+//         console.log(e);
+//         return pass;
+//     }
+// }
+
+// function draw(ctx, options) {
+//     if (options.fillStyle) {
+//         ctx.fillStyle = options.fillStyle;
+//     }
+//     ctx.fillRect(options.x, options.y, options.w, options.h);
+// }
